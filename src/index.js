a28c00670a05f5f278ef3d9fb4b97954764f2a9f
Leonardo Zarzar <leonardozarzar@MacBook-Pro-de-Leonardo.local>
2023-09-14 10:07:27 -0300

    Passando as configurações de rotas para o app

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const cors = require('cors')
+const router = require('./routes');
 
 const app = express();
 const port = 3000;
 
 app.use(cors({origin: "*"}));
+
+router(app);
 
 app.listen(port, () => console.log('Ouvindo na porta 3000.'));
 
